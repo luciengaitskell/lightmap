3ce884191c3d7a07b1cd0fd8ebe6039d8ddc8748
Luc Gaitskell <me@lucg.xyz>
2025-12-24 21:43:40 -0500

    Move defines to header

--- src/main.cpp
+++ src/main.cpp
@@ -13,51 +13,10 @@
 #include <Arduino.h>
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
-// HUB75E pinout
-// R1 | G1
-// B1 | GND
-// R2 | G2
-// B2 | E
-//  A | B
-//  C | D
-// CLK| LAT
-// OE | GND
-
-#define R1 42
-#define G1 41
-#define BL1 40
-#define R2 38
-#define G2 39
-#define BL2 37
-#define CH_A 45
-#define CH_B 36
-#define CH_C 48
-#define CH_D 35
-#define CH_E 21
-#define CLK 2
-#define LAT 47
-#define OE 14
-
-#define PANEL_WIDTH 64
-#define PANEL_HEIGHT 64
-
-#define PANELS_NUMBER 1
-
-#define PANE_WIDTH PANEL_WIDTH *PANELS_NUMBER
-#define PANE_HEIGHT PANEL_HEIGHT
-#define NUM_LEDS PANE_WIDTH *PANE_HEIGHT
-
 MatrixPanel_I2S_DMA *matrix = nullptr;
 
 unsigned long t1, t2;
 
-static inline uint8_t tri8(uint8_t t) {
-  uint8_t v = (t & 0x7F) << 1;
-  return (t & 0x80) ? (uint8_t)(255 - v) : v;
-}
-
-#define MASK_GROUP_SIZE (sizeof(uint64_t) * 8)
-#define MASK_WIDTH PANEL_WIDTH / MASK_GROUP_SIZE
 template <typename F>
 void write_mask(const uint64_t (&mask)[PANEL_HEIGHT][MASK_WIDTH], F draw_fn) {
   for (uint16_t y = 0; y < PANEL_HEIGHT; y++) {
@@ -74,38 +33,6 @@ void write_mask(const uint64_t (&mask)[PANEL_HEIGHT][MASK_WIDTH], F draw_fn) {
   }
 }
 
-const uint64_t watermask[PANEL_HEIGHT][MASK_WIDTH] = {
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0},
-    {0x0000FFFFFFFFFF00},
-    {0x0000FFFFFFFFFFF0},
-    {0x0000FFFFFFFFFFF8},
-    {0x0000FFFFFFFFFFFC},
-    {0x0000FFFFFFFFFFFE},
-    {0x0000FFFFFFFFFFFF},
-    {0x00000FFFFFFFFFFF},
-    {0x00000FFFFFFFFFFF},
-    {0x000000FFFFFFFFFF},
-    {0x000000FFFFFFFFFF},
-    {0x000000FFFFFFFFFF},
-};
 uint16_t water_phase = 0;
 
 void water_draw_pixel(uint8_t x, uint8_t y) {
--- src/main.h
+++ src/main.h
@@ -1,3 +1,78 @@
 #include <FastLED.h>
 
 #define BAUD_RATE 115200 // serial debug port baud rate
+
+static inline uint8_t tri8(uint8_t t) {
+  uint8_t v = (t & 0x7F) << 1;
+  return (t & 0x80) ? (uint8_t)(255 - v) : v;
+}
+
+// HUB75E pinout
+// R1 | G1
+// B1 | GND
+// R2 | G2
+// B2 | E
+//  A | B
+//  C | D
+// CLK| LAT
+// OE | GND
+
+#define R1 42
+#define G1 41
+#define BL1 40
+#define R2 38
+#define G2 39
+#define BL2 37
+#define CH_A 45
+#define CH_B 36
+#define CH_C 48
+#define CH_D 35
+#define CH_E 21
+#define CLK 2
+#define LAT 47
+#define OE 14
+
+#define PANEL_WIDTH 64
+#define PANEL_HEIGHT 64
+
+#define PANELS_NUMBER 1
+
+#define PANE_WIDTH PANEL_WIDTH *PANELS_NUMBER
+#define PANE_HEIGHT PANEL_HEIGHT
+#define NUM_LEDS PANE_WIDTH *PANE_HEIGHT
+
+#define MASK_GROUP_SIZE (sizeof(uint64_t) * 8)
+#define MASK_WIDTH PANEL_WIDTH / MASK_GROUP_SIZE
+
+const uint64_t watermask[PANEL_HEIGHT][MASK_WIDTH] = {
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0},
+    {0x0000FFFFFFFFFF00},
+    {0x0000FFFFFFFFFFF0},
+    {0x0000FFFFFFFFFFF8},
+    {0x0000FFFFFFFFFFFC},
+    {0x0000FFFFFFFFFFFE},
+    {0x0000FFFFFFFFFFFF},
+    {0x00000FFFFFFFFFFF},
+    {0x00000FFFFFFFFFFF},
+    {0x000000FFFFFFFFFF},
+    {0x000000FFFFFFFFFF},
+    {0x000000FFFFFFFFFF},
+};
